7e286664767a63428e290b1a679562749e66f997
Piotr Nowak <peterek99914@interia.pl>
2021-04-09 13:08:17 +0200

    Dodaj pliki projektów.

---
+++ SPD3/SPD3.cpp
@@ -0,0 +1,37 @@
+﻿// program do zadania 3 z SPD - Algorytm NEH
+#include <iostream>
+#include <fstream>
+using namespace std;
+int cmax(int N, int M, int* P, int* X, int xsize)
+{
+	return 0;
+}
+void wyswietl(int N, int M, int* P)
+{
+	for (int i = 0; i < N * M; i++)
+	{
+		cout << P[i] << " ";
+		if (i % M == 0)
+		{
+			cout << endl;
+		}
+	}
+}
+int main()
+{
+	int N, M, P[10000];
+	string s;
+	ifstream f("data.txt");
+	while (s != "data.001:")
+	{
+		f >> s;
+	}
+	f >> N >> M;
+	for (int i = 0; i < N * M; i++)
+	{
+		f >> P[i];
+	}
+	wyswietl(N, M, P);
+	cin.get();
+	f.close();
+}
