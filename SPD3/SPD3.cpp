55c79a9bee0293b35f24d59208e7942686c0adad
Piotr Nowak <peterek99914@interia.pl>
2021-04-09 13:19:41 +0200

    Poprawnie wyświetlana kolejność wykonywania zadań

--- SPD3/SPD3.cpp
+++ SPD3/SPD3.cpp
@@ -2,36 +2,32 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-int cmax(int N, int M, int* P, int* X, int xsize)
+int cmax(int N, int M, int* P, int* X, int xsize) //funkcja licząca długość wykonywania wszystkich zadań
 {
 	return 0;
 }
-void wyswietl(int N, int M, int* P)
+void wyswietl(int N, int M, int* P)	 //funkcja wyświetlająca, w jakiej kolejności są wykonywane zadania
 {
-	for (int i = 0; i < N * M; i++)
+	for (int i = 0; i < N * M; i+=M)
 	{
-		cout << P[i] << " ";
-		if (i % M == 0)
-		{
-			cout << endl;
-		}
+		cout << (i / M) + 1 << " ";
 	}
 }
 int main()
 {
 	int N, M, P[10000];
-	string s;
-	ifstream f("data.txt");
-	while (s != "data.001:")
+	string s;	
+	ifstream f("data.txt");		// otworzenie strumienia wczytywania danych z pliku
+	while (s != "data.001:")	//szukanie odpowiedniego zbioru danych
 	{
 		f >> s;
 	}
-	f >> N >> M;
-	for (int i = 0; i < N * M; i++)
+	f >> N >> M;		//wczytanie ilości zadań oraz ilości maszyn
+	for (int i = 0; i < N * M; i++)		//wczytywanie parametrów kolejnych zadań
 	{
-		f >> P[i];
+		f >> P[i];	
 	}
-	wyswietl(N, M, P);
-	cin.get();
-	f.close();
+	wyswietl(N, M, P);		//wyświetlenie kolejności wykonywania zadań
+	cin.get();				// czekanie z skończeniem działania programu
+	f.close();			//zamknięcie strumienia wczytywania danych z pliku
 }
