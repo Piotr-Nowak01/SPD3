6696eca62871f0d4661aa2628eb7b5c31ea6d055
Piotr Nowak <peterek99914@interia.pl>
2021-04-09 14:53:08 +0200

    Algorytm NEH jest do poprawy

--- SPD3/SPD3.cpp
+++ SPD3/SPD3.cpp
@@ -2,20 +2,70 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-int cmax(int N, int M, int* P, int* X, int xsize) //funkcja licząca długość wykonywania wszystkich zadań
+int cmax(int N, int M, int *P, int *X) //funkcja licząca długość wykonywania zadań
 {
-	return 0;
-}
-void wyswietl(int N, int M, int* P)	 //funkcja wyświetlająca, w jakiej kolejności są wykonywane zadania
-{
-	for (int i = 0; i < N * M; i+=M)
+	int T[100];
+	for (int m = 0; m <= M; m++)
 	{
-		cout << (i / M) + 1 << " ";
+		T[m] = 0;
 	}
+	for (int n = 0; n < N; n++)
+	{
+		for (int m = 1; m <= M; m++)
+		{
+			T[m] = max(T[m],T[m-1]) + P[(m-1)+X[n]*M];
+		}
+	}
+	return T[M];
+}
+int NEH(int N, int M, int* P, int* X)
+{
+	int* W = new int[N];
+	for (int c = 0; c < N; c++)	 //przypisanie wagi każdemu zadaniu
+	{
+		for (int d = 0; d < M; d++)
+		{
+			W[c] += P[c + d];
+		}
+	}
+	for (int b = 0; b < N - 1; b++)		//sortowanie zadań wagami
+	{
+		for (int a = 0; a < N - 1; a++)
+		{
+			if (W[a] < W[a + 1])
+			{
+				swap(W[a], W[a + 1]);
+				swap(X[a], X[a + 1]);
+			}
+		}
+	}
+	delete[]W;
+	for (int n = 0; n < N; n++)		//wstawianie na odpowiednią pozycję
+	{
+		int bestP = -1, bestCmax = 9999999;
+		for (int p = n; p >= 0; p--)
+		{
+			int tmp = cmax(n+1, M, P, X);
+			if (bestCmax >= tmp)
+			{
+				bestCmax = tmp;
+				bestP = p;
+			}
+			if (p)
+			{
+				swap(X[p], X[p - 1]);
+			}
+		}
+		for (int i = 0; i < bestP; i++)
+		{
+			swap(X[i], X[i + 1]);
+		}
+	}
+	return cmax(N, M, P, X);
 }
 int main()
 {
-	int N, M, P[10000];
+	int N, M, P[10000],X[1000];
 	string s;	
 	ifstream f("data.txt");		// otworzenie strumienia wczytywania danych z pliku
 	while (s != "data.001:")	//szukanie odpowiedniego zbioru danych
@@ -27,7 +77,15 @@ int main()
 	{
 		f >> P[i];	
 	}
-	wyswietl(N, M, P);		//wyświetlenie kolejności wykonywania zadań
+	for (int i = 0; i < N; i++)
+	{
+		X[i] = i;
+	}
+	cout <<NEH(N, M, P, X)<<endl;
+	for (int i = 0; i < N; i++)
+	{
+		cout << X[i] + 1<<" ";
+	}
 	cin.get();				// czekanie z skończeniem działania programu
 	f.close();			//zamknięcie strumienia wczytywania danych z pliku
 }
